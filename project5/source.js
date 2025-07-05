26fb89b5e9e75003a3a3eb537e1ee97aa0973a41
the-sadanand <thesadanand.toponed0@gmail.com>
2025-07-05 12:06:31 +0530

    project 5

---
+++ project5/source.js
@@ -0,0 +1,16 @@
+const insert=document.querySelector('#insert')
+window.addEventListener('keydown',(e)=>{
+    insert.innerHTML=`<table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key===" "?'space':e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+ 
+</table>`
+})
